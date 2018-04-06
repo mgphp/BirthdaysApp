0a781a0caa4d24579b50b09ddce21ca39212f597
mgphp <mark.grogan1978@gmail.com>
2018-04-06 08:32:56 +0100

    Add some data

---
+++ src/scripts/data/birthdays.js
@@ -0,0 +1,36 @@
+const birthdays = [
+  {
+    "name": "John Doe",
+    "birthday": "1981-08-02"
+  },
+  {
+    "name": "Katie Smith",
+    "birthday": "1973-05-20"
+  },
+  {
+    "name": "Anna Jackson",
+    "birthday": "1993-10-15"
+  },
+  {
+    "name": "Albert Einstein",
+    "birthday": "1981-08-02"
+  },
+  {
+    "name": "Isaac Newton",
+    "birthday": "1973-05-20"
+  },
+  {
+    "name": "Johannes PLank",
+    "birthday": "1993-10-15"
+  },
+  {
+    "name": "John Kennedy",
+    "birthday": "1993-10-15"
+  },
+  {
+    "name": "Winston Churchill",
+    "birthday": "1874-11-30"
+  }
+];
+
+export default birthdays;
